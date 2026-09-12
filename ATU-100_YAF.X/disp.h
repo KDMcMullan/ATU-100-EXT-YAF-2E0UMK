664e7395ff1c74059c4e00ef7a1ca2bc028d4559
KDM <ken@mcmullanfamily.org.uk>
2026-09-12 23:22:28 +0100

    Add OLED screensaver functions

--- ATU-100_YAF.X/disp.h
+++ ATU-100_YAF.X/disp.h
@@ -1,8 +1,21 @@
+/*
+ * ATU-100_EXT_YAF 
+ * Yet Another Firmware
+ * Author (presumably): Sven, DG4SN
+ *
+ * Display prototypes
+ * (Presumably) created around March 2022
+ *
+ * Modified 12-Sep-2026 2E0UMK
+ * Added functions for OLED screensaver.
+ * 
+ */
+
 /* 
  * File:   disp.h
  * Author: sven
  *
- * Created on 23. März 2022, 15:38
+ * Created on 23. MÃ¤rz 2022, 15:38
  */
 
 #ifndef DISP_H
@@ -24,6 +37,9 @@ void DISP_SWR(uint8_t col, uint8_t row, int16_t centiSWR, uint8_t invert);
 void DISP_On(void);
 void DISP_Off(void);
 void DISP_Clr(void);
+void DISP_DrawVLine(uint8_t x, uint8_t state);
+void DISP_MoveVLine(uint8_t old_col, uint8_t new_col);
+void DISP_RenderScreenSaver(void);
 
 
 #ifdef	__cplusplus
