74921688ddf096f48e20e351742cc3cb5bac4eab
KDM <ken@mcmullanfamily.org.uk>
2026-09-12 23:25:07 +0100

    Explicitly type tunestate_t
    Updated tune.h to explicitly type tunestate_t as uint8_t for memory efficiency.

--- ATU-100_YAF.X/tune.h
+++ ATU-100_YAF.X/tune.h
@@ -1,8 +1,18 @@
-/* 
+/**
+ * ATU-100_EXT_YAF 
+ * Yet Another Firmware
+ * (presumably) by DG4SN, Sven
+ *
+ * Created on 30 Jun 2022
  * File:   tune.h
+ * 
+ * Modified 12-Sep-2026 2E0UMK
+ * Explicitly typed tunestate_t as U8 to save variable space
+ * 
+ */
+/* 
  * Author: sven
  *
- * Created on 30. Juni 2022, 19:50
  */
 
 #ifndef TUNE_H
@@ -21,15 +31,16 @@ extern "C"
 
   
  
-  
-typedef enum
+typedef uint8_t tunestate_t; // force U8 (default is likely U16)
+
+enum
 {
   TUNE_IDLE=1,
   TUNE_RUN=2,
   TUNE_BREAK=3,
   TUNE_PWRLOW=4,        
   TUNE_READY=5           
-} tunestate_t;  
+};  
   
 
 
